d9cb3dc2a10eb38a342039637e082de41a8edb83
María Alejandra G <alis990@hotmail.com>
2019-08-11 20:29:59 -0500

    Logout and Permisions

--- MyVet.Web/Controllers/AccountController.cs
+++ MyVet.Web/Controllers/AccountController.cs
@@ -37,9 +37,17 @@ namespace MyVet.Web.Controllers
                     return RedirectToAction("Index", "Home");
                 }
                 ModelState.AddModelError(string.Empty, "User or password not valid.");
+                model.Password = string.Empty;
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _userHelper.LogoutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }

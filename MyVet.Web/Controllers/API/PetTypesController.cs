4bfc5950af0e938878fa10105a18531a996a1c01
María Alejandra G <alis990@hotmail.com>
2019-08-25 23:41:39 -0500

    Basic API Controller

---
+++ MyVet.Web/Controllers/API/PetTypesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyVet.Web.Data;
+using MyVet.Web.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PetTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PetTypes
+        [HttpGet]
+        public IEnumerable<PetType> GetPetTypes()
+        {
+            return _context.PetTypes;
+        }
+
+    }
+}
